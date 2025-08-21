8d9dd3b65cf813f62ce2ba0fa25b694388f96403
Anushka Samaranayake <123594904+AnushkaSamaranayake@users.noreply.github.com>
2025-08-21 15:20:54 +0530

    added a toggle switch

--- frontend/src/components/Transformers/Head.jsx
+++ frontend/src/components/Transformers/Head.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
-const Head = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const Head = ({ activeTable, setActiveTable}) => {
 
-export default Head
+    return (
+        <div className='flex flex-row mx-5 mt-5 items-center justify-between'>
+            <div className='flex flex-row'>
+                <h2 className='text-xl font-semibold text-blue-800'>Transformer</h2>
+                <button className='ml-5 px-4 py-1 bg-blue-500 text-white rounded-lg text-sm' onClick={() => navigate('#')}>Add transformer</button>
+            </div>
+            <div className='flex flex-row'>
+                <button onClick={() => setActiveTable("transformers")} className={`px-4 py-1 ${activeTable === "transformers" ? "bg-blue-500 text-white" : "bg-gray-100 text-gray-700"} rounded-sm bg-blue-500 text-white text-sm`}>Transformers</button>
+
+                <button onClick={() => setActiveTable("inspections")} className={`px-4 py-1 ${activeTable === "inspections" ? "bg-blue-500 text-white" : "bg-gray-100 text-gray-700"} rounded-sm bg-blue-500 text-white text-sm`}>Inspections</button>
+            </div>
+        </div>
+  );
+};
+
+export default Head;
