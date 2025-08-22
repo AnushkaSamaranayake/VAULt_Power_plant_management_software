1d761dd0fa59df2213b2b1fb89cf836bd8b522f6
Anushka Samaranayake <123594904+AnushkaSamaranayake@users.noreply.github.com>
2025-08-22 12:05:48 +0530

    change footer elements

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 
 const Footer = () => {
-  return (
-    <footer className='bottom-0 left-0 right-0 w-full bg-gray-800 text-white py-4'>
-      <div className='container mx-auto text-center'>
-        <p>&copy; 2023 Transformer Management Systems. All rights reserved.</p>
-      </div>
-    </footer>
-  )
+
+    const [time, setTime] = useState(new Date());
+    
+        useEffect(() => {
+            const interval = setInterval(() => {
+                setTime(new Date());
+            }, 60000);
+    
+            return () => clearInterval(interval);
+            }, []);
+
+    return (
+        <footer className='bottom-0 left-0 right-0 w-full bg-gray-800 text-white py-4'>
+        <div className=' flex flex-col container mx-auto text-center'>
+            <p className='text-md mb-4'>&copy; 2025 Transformer Management Systems. All rights reserved.</p>
+            <p className='text-xs'>Last updated: {time.toLocaleTimeString()}</p>
+        </div>
+        </footer>
+    )
 }
 
 export default Footer;
