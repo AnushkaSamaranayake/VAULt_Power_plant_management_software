7ca5b94d01e5698cc1b2ca7e1bef6d959c7ea8aa
Anushka Samaranayake <123594904+AnushkaSamaranayake@users.noreply.github.com>
2025-08-23 15:59:21 +0530

    rename the transfordetail folder to inspections details

--- frontend/src/pages/InspectionDetails.jsx
+++ frontend/src/pages/InspectionDetails.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { useParams } from 'react-router'
 import inspections from '../constants/inspections.json'
 import { useState } from 'react'
-import Head from '../components/TransformerDetails/Head'
-import ImageUpload from '../components/TransformerDetails/ImageUpload'
+import Head from '../components/InspectionDetails/Head'
+import ImageUpload from '../components/InspectionDetails/ImageUpload'
 import Footer from '../components/Footer'
 
 const InspectionDetails = () => {
---
+++ frontend/src/pages/TransformerDetails.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useParams } from 'react-router'
+import inspections from '../constants/inspections.json'
+import { useState } from 'react'
+import Head from '../components/InspectionDetails/Head'
+import ImageUpload from '../components/InspectionDetails/ImageUpload'
+import Footer from '../components/Footer'
+
+const TransformerDetails = () => {
+
+
+    return (
+        <div>
+            <h1>Hello Transformer Details</h1>
+        </div>
+    );   
+};
+
+export default TransformerDetails;
