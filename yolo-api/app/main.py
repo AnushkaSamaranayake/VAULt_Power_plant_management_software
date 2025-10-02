db092aed076fc3331ff3971bfc7235a694614b66
AnushkaSamaranayake <anushkasandeepa522@gmail.com>
2025-10-02 14:14:56 +0530

    added the API reference

--- yolo-api/app/main.py
+++ yolo-api/app/main.py
@@ -8,9 +8,10 @@ def root():
     return {"message": "Welcome to the YOLOv8 Thermal Anomaly Detection API"}
 
 @app.post("/inference")
+#Sample API call: "http://localhost:8000/inference?conf_threshold=0.80"\ -F "file=@test\images\T9_faulty_001_jpg.rf.94b2e9d6b21a1109f1289da38e134c3f.jpg"
 async def inference(
     file: UploadFile = File(...),
-    conf_threshold: float = Query(0.25, description="Confidence threshold for predictions")
+    conf_threshold: float = Query(0.10, description="Confidence threshold for predictions")
 ):
     
     #Readfile
